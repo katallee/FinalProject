ec42387f0b5bf4bfe85bc390b97b8eb053a57adf
Kat Allee <kallee@butlercc.edu>
2021-04-28 09:09:32 -0500

    Initial Commit

---
+++ FinalProject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FinalProject
+//
+//  Created by Student on 4/28/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FinalProject/FinalProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalProjectApp.swift
+//  FinalProject
+//
+//  Created by Student on 4/28/21.
+//
+
+import SwiftUI
+
+@main
+struct FinalProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
